27855ba77856bec2041bfcc9b95ebc091cfc120e
Alexander Ryzhakov <aryzhakov87@gmail.com>
2025-08-15 14:18:31 +1000

    Test passed

--- src/test/java/ru/netology/rest/BankDebCardTest.java
+++ src/test/java/ru/netology/rest/BankDebCardTest.java
@@ -1,39 +1,48 @@
 package ru.netology.rest;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
 
 public class BankDebCardTest {
     private WebDriver driver;
 
     @BeforeAll
-    public static void setupAll () {
+    public static void setupAll() {
         WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
-    void setup () {
+    void setup() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999");
     }
 
     @AfterEach
-    void tearDown () {
+    void tearDown() {
         driver.quit();
         driver = null;
     }
 
     @Test
-    void shouldSendForm () {
-
+    void shouldSendForm() {
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иванов Иван");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79990009999");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector(".button")).click();
+        WebElement element = driver.findElement(By.cssSelector("[data-test-id='order-success']"));
+        String actual = element.getText().trim();
+        Assertions.assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actual);
+        Assertions.assertTrue(element.isDisplayed());
     }
 }
